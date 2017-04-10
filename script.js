cfb06ed5165f25f777d185c4d1c21b9b4bb4fb4e
Paulina Chmielewska <Paulina Chmielewska>
2017-04-10 20:18:31 +0200

    slider

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
 // story slider
 
-$(document).ready(function() {
-	slider();
-});
-
-function slider() {
-	$('.control-dot').first().addClass('active');
-	$('.s-content').first().addClass('active');
-
-
-	$('.control-dot').click(function() {
-		var $this = $(this),
-			$dotLength = $this.parent().children(),
-			position = $dotLength.index($this);
-
-		$('.control-dot').removeClass('active').eq(position).addClass('active');
-		$('.s-content').removeClass('active').eq(position).addClass('active');
-	});
-};
 
 // scroll top button
 
