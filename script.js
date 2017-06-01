8ec0029c549addcb0288c5cf83389a53aa3f0165
Paulina <panipaulina@gmail.com>
2017-06-01 12:20:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,16 @@
-$(document).ready(function() {
-    
-    /* Every time the window is scrolled ... */
-    $(window).scroll( function(){
-    
-        /* Check the location of each desired element */
-        $('.fadein').each( function(){
-            
+$(document).ready(function() {    
+   $(window).scroll( function(){        
+        $('.fadein').each( function(){            
             var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
-            /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window > bottom_of_object ){
-                
-                $(this).animate({'opacity':'1'},200);
-                    
-            }
-            
-        }); 
-    
-    });
-    
+            var bottom_of_window = $(window).scrollTop() + $(window).height();           
+            if( bottom_of_window > bottom_of_object ){                
+                $(this).animate({'opacity':'1'},200);                   
+            }            
+        });     
+    });    
 });
 
 // scroll top button
-
 var $backBtn = $('#back'),
     windowHeight = $(window).height();
 
@@ -41,11 +28,10 @@ $backBtn.click(function() {
   }, 800);
 });
 
-// scroll top button
-
+// section animations
 $('#header a').click(function(event) {
   event.preventDefault();
-  
+    
   var target = $(this).attr('href'),
       menuItem = $(this),
       targetOffset = $(target).offset().top;
